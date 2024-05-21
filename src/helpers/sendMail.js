5946076694879b32b5864afabb47bdf4a0a21fb7
sammed <ssammed24@gmail.com>
2024-05-21 18:57:22 +0530

    modifications in email template html.

--- src/helpers/sendMail.js
+++ src/helpers/sendMail.js
@@ -2,28 +2,7 @@ const nodemailer = require('nodemailer');
 
 async function sendEmail(to, subject, text) {
     try {
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'mamatakuwarrathod@gmail.com', 
-                pass: 'zuac mnff vnkg wbtd'
-            }
-        });
-        let mailOptions = {
-            from: 'mamatakuwarrathod@gmail.com',
-            to: to,
-            subject: subject, 
-            text: htmlContent 
-        };
-        let info = await transporter.sendMail(mailOptions);
-    } catch (error) {
-        throw new Error('Error sending email');
-    }
-}
-module.exports = {
-    sendEmail
-};
-const htmlContent = `
+        const htmlContent = `
 <!DOCTYPE html>
 <html>
 <head>
@@ -48,3 +27,24 @@ const htmlContent = `
 </body>
 </html>
 `;
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'mamatakuwarrathod@gmail.com', 
+                pass: 'zuac mnff vnkg wbtd'
+            }
+        });
+        let mailOptions = {
+            from: 'mamatakuwarrathod@gmail.com',
+            to: to,
+            subject: subject, 
+            html: htmlContent 
+        };
+        let info = await transporter.sendMail(mailOptions);
+    } catch (error) {
+        throw new Error('Error sending email');
+    }
+}
+module.exports = {
+    sendEmail
+};
